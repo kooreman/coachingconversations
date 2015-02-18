ae461822a9d6a9566b7dd3747cd3bc18666dd2fd
Martijn Kooreman <m.kooreman@gmail.com>
2015-02-18 10:18:25 +0100

    texten

--- app/scripts/doon.js
+++ app/scripts/doon.js
@@ -17,7 +17,7 @@
     $('#contactform').submit(function(e) {
         e.preventDefault();
         $.ajax({
-            url: '//formspree.io/info@coachingconversations.com',
+            url: '//formspree.io/info@coachingconversations.nl',
             method: 'POST',
             data: {
                 'naam': $('#naam').val(),
